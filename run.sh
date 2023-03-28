kaushikravichandran/astra-sim

Implement graph-based execution engine

run.sh
@@ -1,0 +1,12 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "$(realpath $0)")
+BINARY="${SCRIPT_DIR:?}"/build/astra_analytical/build/AnalyticalAstra/bin/AnalyticalAstra
+WORKLOAD="${SCRIPT_DIR:?}"/extern/graph_frontend/chakra/eg_generator/one_comp_node
+SYSTEM="${SCRIPT_DIR:?}"/inputs/system/sample_fully_connected_sys.txt
+NETWORK="${SCRIPT_DIR:?}"/inputs/network/analytical/fully_connected.json
+
+"${BINARY}" \
+  --workload-configuration="${WORKLOAD}" \
+  --system-configuration="${SYSTEM}" \
+  --network-configuration="${NETWORK}"

UPC-Qualcomm/astra-sim-opt

run.sh
@@ -1,0 +1,12 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "$(realpath $0)")
+BINARY="${SCRIPT_DIR:?}"/build/astra_analytical/build/AnalyticalAstra/bin/AnalyticalAstra
+WORKLOAD="${SCRIPT_DIR:?}"/extern/graph_frontend/chakra/eg_generator/twoCompNodesDependent
+SYSTEM="${SCRIPT_DIR:?}"/inputs/system/sample_fully_connected_sys.txt
+NETWORK="${SCRIPT_DIR:?}"/inputs/network/analytical/fully_connected.json
+
+"${BINARY}" \
+  --workload-configuration="${WORKLOAD}" \
+  --system-configuration="${SYSTEM}" \
+  --network-configuration="${NETWORK}"