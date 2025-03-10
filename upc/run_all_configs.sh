c7b2c0625fee2eb0f6cab8a1a3d0eac9a7e16d81
Mohammad Nasser <mohammad.nasser@upc.edu>
2025-03-10 17:36:03 +0100

    Add scripts to run multiple simulations on generated work loads

---
+++ upc/run_all_configs.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+
+workload_configuration="./workload/GPT3"
+memory_config="./configuration/RemoteMemory.json"
+
+
+#2D_Torus
+python run_astrasim.py \
+--workload_dir workload_configuration \
+--system ./configuration/2D_Torus_sys.json \
+--network ./configuration/2D_Torus.yml \
+--memory memory_config \
+--output_dir ./output/GPT3/2D_Torus
+
+#3D_Torus
+python run_astrasim.py \
+--workload_dir ./workload/GPT3 \
+--system ./configuration/3D_Torus_sys.json \
+--network ./configuration/3D_Torus.yml \
+--memory ./configuration/RemoteMemory.json \
+--output_dir ./output/GPT3/3D_Torus
+
+
+#DGX_H100
+python run_astrasim.py \
+--workload_dir ./workload/GPT3 \
+--system ./configuration/DGX_H100.json \
+--network ./configuration/DGX_H100.yml \
+--memory ./configuration/RemoteMemory.json \
+--output_dir ./output/GPT3/DGX_H100
+
+
+#DGX1
+python run_astrasim.py \
+--workload_dir ./workload/GPT3 \
+--system ./configuration/DGX1_sys.json \
+--network ./configuration/DGX1.yml \
+--memory ./configuration/RemoteMemory.json \
+--output_dir ./output/GPT3/DGX1
+
+
+#Dragonfly
+python run_astrasim.py \
+--workload_dir ./workload/GPT3 \
+--system ./configuration/Dragonfly_sys.json \
+--network ./configuration/Dragonfly.yml \
+--memory ./configuration/RemoteMemory.json \
+--output_dir ./output/GPT3/Dragonfly
+
+
+#FullyConnected
+python run_astrasim.py \
+--workload_dir ./workload/GPT3 \
+--system ./configuration/FullyConnected_sys.json \
+--network ./configuration/FullyConnected.yml \
+--memory ./configuration/RemoteMemory.json \
+--output_dir ./output/GPT3/FullyConnected
+
+#Ring
+python run_astrasim.py \
+--workload_dir ./workload/GPT3 \
+--system ./configuration/Ring_sys.json \
+--network ./configuration/Ring.yml \
+--memory ./configuration/RemoteMemory.json \
+--output_dir ./output/GPT3/Ring
+
+#Switch
+python run_astrasim.py \
+--workload_dir ./workload/GPT3 \
+--system ./configuration/Switch_sys.json \
+--network ./configuration/Switch.yml \
+--memory ./configuration/RemoteMemory.json \
+--output_dir ./output/GPT3/Switch
+
+
---
+++ upc/run_astrasim.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+import os, subprocess, multiprocessing
+import argparse
+
+def run_command(command, cwd=None):
+    result=subprocess.run(command, shell=True, cwd=cwd)
+    return result.returncode == 0
+
+def list_workloads(root):
+    files = os.listdir(root)
+    filtered = list()
+    for file in files:
+        if file.endswith(".0.et"):
+            filtered.append(os.path.join(root, file[:-5]))
+    return filtered
+
+def run_astrasim(workload_path, system, network, memory, output_dir):
+    astrasim_root = "/home/mohammad/spain/experiments/astra-sim"
+    if astrasim_root is None:
+        raise Exception(f"please specify astrasim folder path at variable astrasim_root at {__file__}:run_astrasim()")
+    file_dir = os.path.split(os.path.abspath(__file__))[0]
+    astrasim_bin = os.path.join(astrasim_root, "build/astra_analytical/build/bin/AstraSim_Analytical_Congestion_Unaware")
+
+    
+    system = os.path.join(file_dir, system)
+    network = os.path.join(file_dir, network)
+    memory = os.path.join(file_dir, memory)
+    os.makedirs(os.path.join(file_dir, output_dir), exist_ok=True)
+    log = os.path.join(file_dir, output_dir, os.path.split(workload_path)[1]+".log")
+    cmd = f"{astrasim_bin} --system-configuration={system} --workload-configuration={workload_path} --network-configuration={network} --remote-memory-configuration={memory} --comm-group-configuration={workload_path}.json > {log}"
+    print(cmd)
+    success = run_command(cmd)
+    if not success:
+        return cmd
+    return ""
+    
+
+from functools import partial
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--workload_dir", type=str, help="The folder containing the workload", required=True, default="Default")
+    parser.add_argument("--system", type=str, help="The folder containing the workload", required=True, default="Default")
+    parser.add_argument("--network", type=str, help="The folder containing the workload", required=True, default="Default")
+    parser.add_argument("--memory", type=str, help="The folder containing the workload", required=True, default="Default")
+    parser.add_argument("--output_dir", type=str, help="The folder containing the workload", required=True, default="Default")
+    args = parser.parse_args()
+
+    design_space = list_workloads(str(args.workload_dir))
+    func = partial(run_astrasim, system = args.system, network = args.network, memory = args.memory, output_dir = args.output_dir)
+
+    with multiprocessing.Pool(int(multiprocessing.cpu_count()*0.95)) as pool:
+        failed_cmds=pool.map(func, design_space)
+        print("\n\nrunfails:")
+        for cmd in failed_cmds:
+            if not cmd == "":
+                print(cmd)
+        
+
