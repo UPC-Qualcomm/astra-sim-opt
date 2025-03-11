f8aa1e2664d929b4be31b932cf170de0846c2d42
Mohammad Nasser <mohammad.nasser@upc.edu>
2025-03-11 16:41:30 +0100

    Add the initial version of the get results script.

---
+++ upc/gather_results.py
@@ -0,0 +1,161 @@
+#!/usr/bin/python3
+import os, subprocess, multiprocessing
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import re
+
+def list_logs(root):
+    files = os.listdir(root)
+    filtered = list()
+    for file in files:
+        if file.endswith(".log"):
+            filtered.append(os.path.join(root, file))
+    return filtered
+
+
+def extract_runtime(log_path):
+    log_filename = os.path.split(log_path)[-1]
+    dp, mp, sp, pp, sharded = log_filename[:-4].split('_')
+    dp, mp, sp, pp, sharded = int(dp), int(mp), int(sp), int(pp), int(sharded)
+    exec_cycles = 0
+    comm_cycles = 0
+    
+    
+   
+    pattern = r"(\d+) cycles, exposed communication (\d+) cycles"
+
+    # Lists to store extracted values
+    execution_cycles = []
+    communication_cycles = []
+
+    with open(log_path, 'r') as f:
+        log_lines = f.read()
+        # Extract runtime value for both the compute and exposed communication
+        # Extract for all NPUs and average them put
+
+        # Find matches in the log data
+        matches = re.findall(pattern, log_lines)
+
+        if len(matches) == 0:
+            return dp, mp, sp, pp, sharded, -1, -1  # Not enough lines in log file
+    
+        # Store extracted values in separate lists
+        for exec_cycles, comm_cycles in matches:
+            execution_cycles.append(int(exec_cycles))
+            communication_cycles.append(int(comm_cycles))
+
+        #print("Execution Cycles:", execution_cycles)
+        #print("Communication Cycles:", communication_cycles)
+        exec_cycles = sum(execution_cycles)/len(execution_cycles)
+        comm_cycles = sum(communication_cycles)/len(communication_cycles)
+
+    return dp, mp, sp, pp, sharded, exec_cycles, comm_cycles
+
+def gather_runtimes(root):
+    logs = list_logs(root)
+    runtimes = None
+    with multiprocessing.Pool() as pool:
+        runtimes = pool.map(extract_runtime, logs)
+    runtimes_dict = dict()
+    for dp, mp, sp, pp, sharded, exec_cycles, comm_cycles in runtimes:
+        runtimes_dict[(dp, mp, sp, pp, sharded)] = [exec_cycles, comm_cycles]
+    return runtimes_dict
+
+def get_fails(runtimes):
+    fail_cases = list()
+    for key in runtimes.keys():
+        if runtimes[key] == -1:
+            fail_cases.append(key)
+        print(key)
+    return fail_cases
+
+def visualize1(runtimes, ssp, sharded):
+    max_runtimes = max(runtimes.values())
+    mat = -1*np.ones((7, 7))
+    # vis all data, x=(dp, mp) y=(sp, pp)
+    for ddp in range(7):
+        x_value = ddp
+        for mmp in range(7):
+            y_value = mmp
+            for ssp in {ssp}:
+                ppp = 6-ddp-mmp-ssp
+                rddp, rmmp = int(2**ddp), int(2**mmp)
+                rssp, rppp = int(2**ssp), int(2**ppp)
+                key = (rddp, rmmp, rssp, rppp, sharded)
+                if not key in runtimes:
+                    mat[x_value, y_value] = -1
+                elif runtimes[key]==-1:
+                    mat[x_value, y_value] = -1
+                else:
+                    mat[x_value, y_value] = runtimes[key]/max_runtimes
+    plt.figure(dpi=120)
+    sns.heatmap(mat)
+    plt.title(f"dp vs mp, x=dp y=mp, sp={ssp}, sharded={sharded}")
+    return plt
+
+def visualize2(runtimes, sharded):
+    max_runtimes = max(runtimes.values())
+    mat = -1*np.ones((7*7, 7))
+    # vis all data, x=(dp, mp) y=(sp, pp)
+    for ddp in range(7):
+        for mmp in range(7):
+            x_value = ddp*7+mmp
+            for ssp in range(7):
+                y_value = ssp
+                ppp = 6-ddp-mmp-ssp
+                rddp, rmmp = int(2**ddp), int(2**mmp)
+                rssp, rppp = int(2**ssp), int(2**ppp)
+                key = (rddp, rmmp, rssp, rppp, sharded)
+                if not key in runtimes:
+                    mat[x_value, y_value] = -1
+                elif runtimes[key]==-1:
+                    mat[x_value, y_value] = -1
+                else:
+                    mat[x_value, y_value] = 1-runtimes[key]/max_runtimes
+    plt.figure(dpi=120)
+    sns.heatmap(mat)
+    plt.title(f"all results, x=(dp, mp) y=sp, sharded={sharded}")
+    return plt
+
+def serialize_results(runtimes, json_filename):
+    def get_jsonable_dict(dict_):
+        ret = dict()
+        for key in dict_.keys():
+            ret[str(key)] = dict_[key]
+        return ret
+    import json
+    f = open(json_filename, 'w')
+    json.dump(get_jsonable_dict(runtimes), f, indent=4)
+    f.close()
+    
+def topk(runtimes, k=10):
+    top_k_items = sorted(runtimes.items(), key=lambda x: -x[1][0] if x[1][0] != -1 else -1e100, reverse=True)[:k]
+    for key, value in top_k_items:
+        print(f"{key}: {value}")
+    return top_k_items
+
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--result_dir", type=str, help="The output file path", required=True)
+    args = parser.parse_args()
+    runtimes = gather_runtimes(args.result_dir)
+    hook = 0
+    #for sp in range(7):
+    #    plt1 = visualize1(runtimes, sp, 0)
+    #    plt.savefig(f"dpvsmp_{sp}_ns.png")
+    #    plt1 = visualize1(runtimes, sp, 1)
+    #    plt.savefig(f"dpvsmp_{sp}_s.png")
+    #plt1 = visualize2(runtimes, 0)
+    #plt.savefig(f"all_ns.png")
+    #plt1 = visualize2(runtimes, 1)
+    #plt.savefig(f"all_s.png")
+
+    serialize_results(runtimes, "./results.json")
+    
+    print("top20:")
+    topk(runtimes, k=20)
+    print("\n\n\nfail cases")
+    get_fails(runtimes)
