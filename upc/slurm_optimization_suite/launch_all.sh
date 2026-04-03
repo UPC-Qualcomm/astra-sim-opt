1ba3ad83046358f02cc08facb797484a3c1bd170
Mohammad Nasser <mohammad.nasser@upc.edu>
2026-04-04 00:46:11 +0200

    Increase network cache and memory requirements on sert

--- upc/slurm_optimization_suite/launch_all.sh
+++ upc/slurm_optimization_suite/launch_all.sh
@@ -32,7 +32,7 @@ MAX_RETRIES_PER_EXPERIMENT=3
 
 # Global defaults (can be overridden per experiment in config.env)
 DEFAULT_CORES_PERCENT=32
-DEFAULT_MEM_PER_CORE_GB=4
+DEFAULT_MEM_PER_CORE_GB=10
 MAX_CPUS_PER_EXPERIMENT=1
 MIN_CPUS_PER_EXPERIMENT=1   # Never schedule fewer than this many CPUs; skip node if memory can't fit even this many
 
